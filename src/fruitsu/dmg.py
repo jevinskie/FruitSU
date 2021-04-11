2a7a707d433a8b044683abd88a403d94d57b851a
Jevin Sweval <jevinsweval@gmail.com>
2021-04-11 12:55:02 -0700

    pep8

--- src/fruitsu/dmg.py
+++ src/fruitsu/dmg.py
@@ -2,12 +2,15 @@
 
 import sys
 
+
 def dmg_func():
     print('dmg_func()')
+
 
 def dmg_main(dmg_path):
     print(f'dmg_main(\'{dmg_path}\')')
     return 0
+
 
 if __name__ == '__main__':
     sys.exit(dmg_main(sys.argv[1]))
--- tests/fruitsu/test_dmg.py
+++ tests/fruitsu/test_dmg.py
@@ -4,14 +4,17 @@ import sys
 
 import pytest
 
+
 def inc(x):
     return x + 1
+
 
 def test_answer():
     print('test_answer() WOOHOO!!!!')
     assert inc(3) == 4
 
+
 if __name__ == '__main__':
-	args = sys.argv
-	print(f'pytest.main args: {args}')
-	sys.exit(pytest.main(args))
+    args = sys.argv
+    print(f'pytest.main args: {args}')
+    sys.exit(pytest.main(args))
